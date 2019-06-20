677fde50f177ea20c65a95e824f5bd8ddbd08e32
Oleh Vaskiv <olegvaskiw@ukr.net>
2019-06-20 12:41:09 +0300

    2 article

--- src/server/db/migrations/20190530151812_movies.js
+++ src/server/db/migrations/20190530151812_movies.js
@@ -1,13 +1,11 @@
 exports.up = (knex, Promise) => {
-    return knex.schema.createTable('movies', (table) => {
-      table.increments();
-      table.string('name').notNullable().unique();
-      table.string('genre').notNullable();
-      table.integer('rating').notNullable();
-      table.boolean('explicit').notNullable();
-    });
-  };
-  
-  exports.down = (knex, Promise) => {
-    return knex.schema.dropTable('movies');
-  };
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('username').unique().notNullable();
+    table.string('password').notNullable();
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users');
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const passport = require('koa-passport');
 
 const indexRoutes = require('./routes/index');
 const movieRoutes = require('./routes/movies');
@@ -7,10 +9,23 @@ const movieRoutes = require('./routes/movies');
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
+// sessions
+app.keys = ['super-secret-key'];
+app.use(session(app));
+
+// body parser
 app.use(bodyParser());
+
+// authentication
+require('./auth');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
 
+// server
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
